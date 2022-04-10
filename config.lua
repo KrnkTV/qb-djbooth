56289dfd960ca799bc5d25f48c01e37e00f75718
regalonefour <95228586+regalonefour@users.noreply.github.com>
2022-04-09 23:56:32 -0400

    update

--- config.lua
+++ config.lua
@@ -1,12 +1,16 @@
 Config = {}
 
-Config.DefaultVolume = 0.1 -- Accepted values are 0.01 - 1
+Config.DefaultVolume = 0.05 -- Accepted values are 0.01 - 1
 
 Config.Locations = {
     ['vanilla'] = {
-        ['job'] = 'vanilla', -- Required job to use booth
+        ['job'] = 'vu', -- Required job to use booth
         ['radius'] = 30, -- The radius of the sound from the booth
         ['coords'] = vector3(120.52, -1281.5, 29.48), -- Where the booth is located
         ['playing'] = false
     }
+}
+
+Config.Booths = {
+    [1] = vector3(120.15, -1281.57, 29.43),
 }
